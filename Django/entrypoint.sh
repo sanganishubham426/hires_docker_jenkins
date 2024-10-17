d9e55f58b214b94fcc80227cb443a991838857f0
SHUBHAM_SANGANI <shubham.sangani@broaderai.com>
2024-10-17 15:52:03 +0530

    Update entrypoint.sh

--- Django/entrypoint.sh
+++ Django/entrypoint.sh
@@ -1,17 +1,42 @@
-#!/bin/bash
+# #!/bin/bash
 
-# entrypoint.sh
+# # entrypoint.sh
+
+# # Run Django migrations
+# echo "Running migrations..."    
+# python manage.py makemigrations
+
+# python manage.py migrate
+
+# # Collect static files (if applicable)
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+# # Start Gunicorn
+# echo "Starting Gunicorn Server..."
+# exec gunicorn hires.wsgi:application --bind 0.0.0.0:8000
+
+#!/bin/bash
+set -e
+
+# Print a message indicating the script is starting
+echo "Starting entrypoint script..."
+
+# Check if manage.py exists
+if [ ! -f manage.py ]; then
+    echo "manage.py not found! Please ensure you are in the correct directory."
+    exit 1
+fi
 
 # Run Django migrations
-echo "Running migrations..."    
+echo "Running Django migrations..."
 python manage.py makemigrations
-
 python manage.py migrate
 
 # Collect static files (if applicable)
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start Gunicorn
+# Start Gunicorn server
 echo "Starting Gunicorn Server..."
 exec gunicorn hires.wsgi:application --bind 0.0.0.0:8000
